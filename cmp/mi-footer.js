5deb708a78ab07d9494d2795bdb4abd814409562
AngelRodeaWeb <95391067+AngelRodeaWeb@users.noreply.github.com>
2021-12-02 20:35:56 -0600

    Create mi-footer.js

---
+++ cmp/mi-footer.js
@@ -0,0 +1,14 @@
+class MiFooter
+  extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `<p>
+        Copyright &copy; 2021 Rodea Nambo Jose Angel
+      </p>`;
+  }
+}
+
+customElements.define(
+  "mi-footer", MiFooter);
+/* En este JS lo que haces es que se hace un formtato de footer el cual el llamado en cada html para no
+* declararlo indivuaalmente */
